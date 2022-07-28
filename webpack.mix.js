2fe716dcb164ba5fe6ea0ba76e15deab6fa29844
Adrian <kemski12321@gmail.com>
2022-07-28 23:59:44 +0200

    aa

--- webpack.mix.js
+++ webpack.mix.js
@@ -13,6 +13,7 @@ const mix = require('laravel-mix');
 
 mix.js('resources/js/app.js', 'public/js')
     .js('resources/js/delete.js', 'public/js')
+    .js('resources/js/delete_copy.js', 'public/js')
     .sass('resources/sass/app.scss', 'public/css')
     .sourceMaps();
     mix.browserSync('sklep.test');
