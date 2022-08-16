0165ecda82eaf5968c9658f0c557851f147cb3a2
Adrian <kemski12321@gmail.com>
2022-08-16 23:18:31 +0200

    #33

--- app/Http/Controllers/CartController.php
+++ app/Http/Controllers/CartController.php
@@ -4,14 +4,11 @@
 namespace App\Http\Controllers;
 
 
-use App\Dtos\Cart\CartDto;
-use App\Dtos\Cart\CartItemDto;
 use App\Models\Product;
+use App\ValueObject\Cart;
 use Illuminate\Http\JsonResponse;
-use Illuminate\Support\Arr;
 use Illuminate\Support\Facades\Session;
 use Illuminate\View\View;
-
 
 
 class CartController extends Controller
@@ -25,7 +22,7 @@ class CartController extends Controller
      */
     public function index(): View
     {
-        dd(Session::get('cart', new CartDto()));
+        dd(Session::get('cart', new Cart()));
         return view('home');
     }
 
@@ -39,33 +36,12 @@ class CartController extends Controller
     public function store(Product  $product): JsonResponse
 
     {
-        $cart = Session::get('cart', new CartDto());
-        $items = $cart->getItems();
-        if (Arr::exists($items, $product->id)){
-            $items[$product->id]->incrementQuantity();
-        }else{
-            $cartItemDto = $this->getCartItemDto($product);
-
-            $items[$product->id] = $cartItemDto;
-        }
-        $cart->setItems($items);
-        $cart->incrementTotalQuantity();
-        $cart->incrementTotalSum($product->price);
-
-        Session::put('cart', $cart);
+        $cart = Session::get('cart', new Cart());
+        Session::put('cart', $cart->addItem($product));
         return response()->json([
             'status' => 'success'
         ]);
     }
 
-    private function getCartItemDto(Product $product): CartItemDto
-    {
-        $cartItemDto = new CartItemDto();
-        $cartItemDto->setProductId($product->id);
-        $cartItemDto->setName($product->name);
-        $cartItemDto->setPrice($product->price);
-        $cartItemDto->setImagePath($product->image_path);
-        $cartItemDto->setQuantity(1);
-        return $cartItemDto;
-    }
+
 }
---
+++ app/ValueObject/Cart.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\ValueObject;
+
+
+use App\Models\Product;
+use Closure;
+use Illuminate\Support\Collection;
+
+
+class Cart
+{
+    private Collection $items;
+
+
+    /**
+     * @param Collection|empty $items
+     */
+    public function __construct(Collection $items = null){
+        $this->items = $items ?? Collection::empty();
+    }
+
+
+    /**
+     * @return Collection
+     */
+    public function getItems(): Collection
+    {
+        return $this->items;
+    }
+
+    public function addItem(Product $product): Cart
+    {
+        $items = $this->items;
+        $item = $items->first($this->isProductIdSameAsItemProduct($product));
+        if (!is_null($item)){
+            $items = $items->reject($this->isProductIdSameAsItemProduct($product));
+            $newItem = $item->addQuantity($product);
+        }else{
+            $newItem= new CartItem($product);
+        }
+        $items->add($newItem);
+        return new Cart($items);
+    }
+
+    private function isProductIdSameAsItemProduct(Product $product): Closure
+    {
+        return function ($item) use ($product) {
+            return $product->id == $item->getProductId();
+        };
+    }
+}
